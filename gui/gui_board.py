f44f7dfe09d12268be5daaa6235127120e33904e
Jan Holub <jan.holub@1pf.cz>
2022-11-30 16:57:15 +0100

    Added pieces in to model and displayed in gui

--- gui/gui_board.py
+++ gui/gui_board.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QMainWindow
 
 from gui.gui_widgets import GuiTile, GuiLabel
 from model.enums import PieceColor
-from model.pieces import Pawn
+from model.pieces import Pawn, Rook, Knight, Bishop, Queen, King
 
 
 class GuiChessBoard(QWidget):
@@ -39,16 +39,96 @@ class GuiChessBoard(QWidget):
 
     # Example of game init
     def init_game(self):
-        for i in range(len(self.axis_x)):
-            tile = self._chess_tiles[1][i]
-            tile.clear()
-            w_pawn = Pawn(PieceColor.WHITE)
-            tile.set_piece(w_pawn.gui())
+        self.init_pieces_set()
+
+    def init_pieces_set(self):
+        self.init_piece_set_pawns()
+        self.init_piece_set_rooks()
+        self.init_piece_set_knights()
+        self.init_piece_set_bishops()
+        self.init_piece_set_royal()
+
+    def init_piece_set_pawns(self):
+        # White
         for i in range(len(self.axis_x)):
             tile = self._chess_tiles[6][i]
             tile.clear()
+            w_pawn = Pawn(PieceColor.WHITE)
+            tile.set_piece(w_pawn.gui())
+        # Black
+        for i in range(len(self.axis_x)):
+            tile = self._chess_tiles[1][i]
+            tile.clear()
             b_pawn = Pawn(PieceColor.BLACK)
             tile.set_piece(b_pawn.gui())
+
+    def init_piece_set_rooks(self):
+        # White
+        for i in [0, 7]:
+            tile = self._chess_tiles[7][i]
+            tile.clear()
+            w_rook = Rook(PieceColor.WHITE)
+            tile.set_piece(w_rook.gui())
+
+        # Black
+        for i in [0, 7]:
+            tile = self._chess_tiles[0][i]
+            tile.clear()
+            b_rook = Rook(PieceColor.BLACK)
+            tile.set_piece(b_rook.gui())
+
+    def init_piece_set_knights(self):
+        # White
+        for i in [1, 6]:
+            tile = self._chess_tiles[7][i]
+            tile.clear()
+            w_knight = Knight(PieceColor.WHITE)
+            tile.set_piece(w_knight.gui())
+
+        # Black
+        for i in [1, 6]:
+            tile = self._chess_tiles[0][i]
+            tile.clear()
+            b_knight = Knight(PieceColor.BLACK)
+            tile.set_piece(b_knight.gui())
+
+    def init_piece_set_bishops(self):
+        # White
+        for i in [2, 5]:
+            tile = self._chess_tiles[7][i]
+            tile.clear()
+            w_bishop = Bishop(PieceColor.WHITE)
+            tile.set_piece(w_bishop.gui())
+
+        # Black
+        for i in [2, 5]:
+            tile = self._chess_tiles[0][i]
+            tile.clear()
+            b_bishop = Bishop(PieceColor.BLACK)
+            tile.set_piece(b_bishop.gui())
+
+    def init_piece_set_royal(self):
+        # White
+        tile = self._chess_tiles[7][3]
+        tile.clear()
+        w_queen = Queen(PieceColor.WHITE)
+        tile.set_piece(w_queen.gui())
+
+        tile = self._chess_tiles[7][4]
+        tile.clear()
+        w_king = King(PieceColor.WHITE)
+        tile.set_piece(w_king.gui())
+
+        # Black
+        tile = self._chess_tiles[0][3]
+        tile.clear()
+        b_queen = Queen(PieceColor.BLACK)
+        tile.set_piece(b_queen.gui())
+
+        tile = self._chess_tiles[0][4]
+        tile.clear()
+        b_king = King(PieceColor.BLACK)
+        tile.set_piece(b_king.gui())
 
 
 class ChessGUI(QMainWindow):
--- model/pieces.py
+++ model/pieces.py
@@ -14,9 +14,33 @@ class APiece(abc.ABC):
         return self._gui_element
 
 
+# TODO - Rook, Knight, Bishop, Queen, King
 class Pawn(APiece):
     def __init__(self, color: PieceColor):
         super().__init__(color, PieceType.PAWN)
 
 
-# TODO - Rook, Knight, Bishop, Queen, King
+class Rook(APiece):
+    def __init__(self, color: PieceColor):
+        super().__init__(color, PieceType.ROOK)
+
+
+class Knight(APiece):
+    def __init__(self, color: PieceColor):
+        super().__init__(color, PieceType.KNIGHT)
+
+
+class Bishop(APiece):
+    def __init__(self, color: PieceColor):
+        super().__init__(color, PieceType.BISHOP)
+
+
+class Queen(APiece):
+    def __init__(self, color: PieceColor):
+        super().__init__(color, PieceType.QUEEN)
+
+
+class King(APiece):
+    def __init__(self, color: PieceColor):
+        super().__init__(color, PieceType.KING)
+
