8876bb9e31d75c1ca0b50ae00e0ce18a59cc6707
Jan Holub <jan.holub@1pf.cz>
2022-12-06 09:35:33 +0100

    Refactoring for code readability.

--- model/pieces.py
+++ model/pieces.py
@@ -3,7 +3,6 @@ from abc import abstractmethod
 
 from gui.gui_widgets import GuiPiece
 from model.enums import PieceColor, PieceType
-from game.rule_manager import RuleManager
 
 
 class APiece(abc.ABC):
@@ -31,7 +30,8 @@ class Pawn(APiece):
             return self._white_moves(x, y)
         return self._black_moves(x, y)
 
-    def _white_moves(self, x, y):
+    @staticmethod
+    def _white_moves(x, y):
         move_list = list()
 
         if x > 0:
@@ -39,7 +39,8 @@ class Pawn(APiece):
 
         return move_list
 
-    def _black_moves(self, x, y):
+    @staticmethod
+    def _black_moves(x, y):
         move_list = list()
 
         if x < 8:
@@ -69,7 +70,56 @@ class Knight(APiece):
         super().__init__(color, PieceType.KNIGHT)
 
     def moves(self, x, y):
-        return ["A3", "A4"]
+        left = self._get_left(x, y)
+        right = self._get_right(x, y)
+        top = self._get_top(x, y)
+        bottom = self._get_bottom(x, y)
+
+        return top + bottom + left + right
+
+    @staticmethod
+    def _get_top(x, y):
+        lst = list()
+        if x > 0:
+            if y < 6:
+                lst.append((x - 1, y + 2))
+            if y > 1:
+                lst.append((x - 1, y - 2))
+
+        return lst
+
+    @staticmethod
+    def _get_bottom(x, y):
+        lst = list()
+        if x < 7:
+            if y < 6:
+                lst.append((x + 1, y + 2))
+            if y > 1:
+                lst.append((x + 1, y - 2))
+
+        return lst
+
+    @staticmethod
+    def _get_right(x, y):
+        lst = list()
+        if x < 7:
+            if x < 6:
+                lst.append((x + 2, y + 1))
+            if x > 1:
+                lst.append((x - 2, y + 1))
+
+        return lst
+
+    @staticmethod
+    def _get_left(x, y):
+        lst = list()
+        if y > 0:
+            if x > 1:
+                lst.append((x - 2, y - 1))
+            if x < 6:
+                lst.append((x + 2, y - 1))
+
+        return lst
 
 
 class Bishop(APiece):
@@ -77,7 +127,56 @@ class Bishop(APiece):
         super().__init__(color, PieceType.BISHOP)
 
     def moves(self, x, y):
-        return ["A3", "A4"]
+        top_left = self._get_top_left(x, y)
+        top_right = self._get_top_right(x, y)
+        bot_left = self._get_bottom_left(x, y)
+        bot_right = self._get_bottom_right(x, y)
+
+        return top_left + top_right + bot_right + bot_left
+
+    @staticmethod
+    def _get_top_left(x, y):
+        lst = list()
+
+        while x != 0 or y != 0:
+            lst.append((x - 1, y - 1))
+            x -= 1
+            y -= 1
+
+        return lst
+
+    @staticmethod
+    def _get_top_right(x, y):
+        lst = list()
+
+        while x != 0 or y != 7:
+            lst.append((x - 1, y + 1))
+            x -= 1
+            y += 1
+
+        return lst
+
+    @staticmethod
+    def _get_bottom_right(x, y):
+        lst = list()
+
+        while x != 7 or y != 7:
+            lst.append((x + 1, y + 1))
+            x += 1
+            y += 1
+
+        return lst
+
+    @staticmethod
+    def _get_bottom_left(x, y):
+        lst = list()
+
+        while x != 7 or y != 0:
+            lst.append((x + 1, y - 1))
+            x += 1
+            y -= 1
+
+        return lst
 
 
 class Queen(APiece):
@@ -85,7 +184,64 @@ class Queen(APiece):
         super().__init__(color, PieceType.QUEEN)
 
     def moves(self, x, y):
-        return ["A3", "A4"]
+        horizontal = list()
+
+        for space in range(8):
+            if space != x:
+                horizontal.append((space, y))
+            if space != y:
+                horizontal.append((x, space))
+
+        top_left = self._get_top_left(x, y)
+        top_right = self._get_top_right(x, y)
+        bot_left = self._get_bottom_left(x, y)
+        bot_right = self._get_bottom_right(x, y)
+
+        return horizontal + top_left + top_right + bot_right + bot_left
+
+    @staticmethod
+    def _get_top_left(x, y):
+        lst = list()
+
+        while x != 0 or y != 0:
+            lst.append((x - 1, y - 1))
+            x -= 1
+            y -= 1
+
+        return lst
+
+    @staticmethod
+    def _get_top_right(x, y):
+        lst = list()
+
+        while x != 0 or y != 7:
+            lst.append((x - 1, y + 1))
+            x -= 1
+            y += 1
+
+        return lst
+
+    @staticmethod
+    def _get_bottom_right(x, y):
+        lst = list()
+
+        while x != 7 or y != 7:
+            lst.append((x + 1, y + 1))
+            x += 1
+            y += 1
+
+        return lst
+
+    @staticmethod
+    def _get_bottom_left(x, y):
+        lst = list()
+
+        while x != 7 or y != 0:
+            lst.append((x + 1, y - 1))
+            x += 1
+            y -= 1
+
+        return lst
 
 
 class King(APiece):
@@ -93,5 +249,39 @@ class King(APiece):
         super().__init__(color, PieceType.KING)
 
     def moves(self, x, y):
-        return ["A3", "A4"]
+        move_list = list()
+
+        if x > 0:
+            if y > 0:
+                # Top left
+                move_list.append((x - 1, y - 1))
+
+            # Left
+            move_list.append((x - 1, y))
+
+            if y < 7:
+                # Top right
+                move_list.append((x - 1, y + 1))
+
+        if y < 7:
+            # Right
+            move_list.append((x, y + 1))
+
+        if y > 0:
+            # Left
+            move_list.append((x, y - 1))
+
+        if x < 7:
+            if y > 0:
+                # Bottom left
+                move_list.append((x + 1, y - 1))
+
+            # Bottom
+            move_list.append((x + 1, y))
+
+            if y < 7:
+                # Bottom right
+                move_list.append((x + 1, y + 1))
+
+        return move_list
 
