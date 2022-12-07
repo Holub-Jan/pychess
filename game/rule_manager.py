3ae193b09bcd5b7672f96933940c725f75528e56
Jan Holub <jan.holub@1pf.cz>
2022-12-07 11:04:54 +0100

    Knight moves, not takes.

--- game/rule_manager.py
+++ game/rule_manager.py
@@ -131,41 +131,37 @@ class RuleManager:
     def _get_knight_top(self, x, y):
         lst = list()
         if x > 0:
-            if y < 6 and self.board[x - 1][y + 2].color != self.color:
+            if y < 6 and (not self.board[x - 1][y + 2] or self.board[x - 1][y + 2].color != self.color):
                 lst.append((x - 1, y + 2))
-            if y > 1 and self.board[x - 1][y - 2].color != self.color:
+            if y > 1 and (not self.board[x - 1][y - 2] or self.board[x - 1][y - 2].color != self.color):
                 lst.append((x - 1, y - 2))
-
         return lst
 
     def _get_knight_bottom(self, x, y):
         lst = list()
         if x < 7:
-            if y < 6 and self.board[x + 1][y + 2].color != self.color:
+            if y < 6 and (not self.board[x + 1][y + 2] or self.board[x + 1][y + 2].color != self.color):
                 lst.append((x + 1, y + 2))
-            if y > 1 and self.board[x - 1][y - 2].color != self.color:
+            if y > 1 and (not self.board[x - 1][y - 2] or self.board[x - 1][y - 2].color != self.color):
                 lst.append((x + 1, y - 2))
-
         return lst
 
     def _get_knight_right(self, x, y):
         lst = list()
-        if x < 7:
-            if x < 6 and self.board[x + 2][y + 1].color != self.color:
+        if y < 7:
+            if x < 6 and (not self.board[x + 2][y + 1] or self.board[x + 2][y + 1].color != self.color):
                 lst.append((x + 2, y + 1))
-            if x > 1 and self.board[x - 2][y + 1].color != self.color:
+            if x > 1 and (not self.board[x - 2][y + 1] or self.board[x - 2][y + 1].color != self.color):
                 lst.append((x - 2, y + 1))
-
         return lst
 
     def _get_knight_left(self, x, y):
         lst = list()
         if y > 0:
-            if x > 1 and self.board[x - 2][y - 1].color != self.color:
+            if x > 1 and (not self.board[x - 2][y - 1] or self.board[x - 2][y - 1].color != self.color):
                 lst.append((x - 2, y - 1))
-            if x < 6 and self.board[x + 2][y - 1].color != self.color:
+            if x < 6 and (not self.board[x + 2][y - 1] or self.board[x + 2][y - 1].color != self.color):
                 lst.append((x + 2, y - 1))
-
         return lst
 
     def _bishop_moves(self, x, y):
@@ -214,7 +210,7 @@ class RuleManager:
         x += 1
         y += 1
         while x <= 7 and y <= 7 and not self.board[x][y]:
-            lst.append((x + 1, y + 1))
+            lst.append((x, y))
             x += 1
             y += 1
 
@@ -230,7 +226,7 @@ class RuleManager:
         x += 1
         y -= 1
         while x <= 7 and y >= 0 and not self.board[x][y]:
-            lst.append((x + 1, y - 1))
+            lst.append((x, y))
             x += 1
             y -= 1
 
@@ -253,6 +249,7 @@ class RuleManager:
 
         return left + right + top + bot + top_left + top_right + bot_right + bot_left
 
+    # TODO : cognitive complexity high
     def _king_moves(self, x, y):
         move_list = list()
 
